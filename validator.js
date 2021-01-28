67df8d9bb2e205b7ecf44f4207f5a50f9f79e817
Ibrahim Adekunle <adefemi101@gmail.com>
2021-01-28 16:29:37 +0100

    made changes to the custom validator

--- validator.js
+++ validator.js
@@ -8,6 +8,32 @@ const isValidJSONString = (str) => {
     return true;
 }
 
+// Return Condition Check
+const conditionCheck = (data, rule) => {
+    if (rule.condition === 'gte') {
+        return data >= rule.condition_value ? true : false;
+    }
+
+    if (rule.condition === 'gt') {
+        return data > rule.condition_value ? true : false;
+    }
+
+    if (rule.condition === 'eq') {
+        return data === rule.condition_value ? true : false;
+    }
+
+    if (rule.condition === 'neq') {
+        console.log('B', typeof data, typeof rule.condition_value)
+        return data !== rule.condition_value ? true : false;
+    }
+
+    if (rule.condition === 'contains') {
+        const tempArray = []    
+        tempArray.push(data)
+        return tempArray.includes(rule.condition_value) ? true : false;
+    }
+}
+
 // Check if data field is either an array, object or string
 const isValidData = (data) => {
     if (typeof data === 'object' || typeof data === 'string') {
@@ -32,13 +58,33 @@ const getFieldNestingLevel = (obj) => {
 
 // Check match between rule object field and data passed
 const checkRuleDataMatch = (res, payload) => {
-    const { rule } = payload
-    // let rules = Object.values(payload.rule);
-    let data = Object.keys(payload.data)
+    const { rule: { field }, rule, data } = payload
+    let dataArr = Object.keys(payload.data)
+    const fieldToArr = field.split('')
 
-    data.forEach(val => {
-        console.log(val)
-        if (!data.includes(rule.field)) {
+    if (fieldToArr.includes('.')) {
+        // get word before and after fullstop
+        const stopIndex = fieldToArr.indexOf('.')
+        const wordBeforeStop = fieldToArr.slice(0, stopIndex).join('')
+        const wordAfterStop = fieldToArr.slice(stopIndex + 1).join('')
+
+        // Check if data contains nested field
+        if (data[wordBeforeStop]) {
+
+            dataArr.forEach(val => {
+                if (!dataArr.includes(wordBeforeStop)) {
+                    return res.status(400).json({
+                        "message": `field ${wordBeforeStop} is missing from data.`,
+                        "status": "error",
+                        "data": null
+                    })
+                }
+            })
+        }
+        return true
+    }
+    dataArr.forEach(val => { 
+        if (!dataArr.includes(rule.field)) {
             return res.status(400).json({
                 "message": `field ${rule.field} is missing from data.`,
                 "status": "error",
@@ -46,46 +92,75 @@ const checkRuleDataMatch = (res, payload) => {
             })
         }
     })
+    
     return true
 }
 
 // Validate Data with Rule
 // ["data.field"] ["rule.condition"] ["rule.condition_value"]
 // ['eq', 'neq', 'gt', 'gte', 'contains']
-const validateRule = (res, payload) => {
+const validateRuleConditions = (payload) => {
     const rule = payload.rule;
     let data = payload.data
 
-    if (rule.condition === 'gte') {
-        return data[rule.field] >= rule.condition_value ? true : false;
+    const fieldToArr = rule.field.split('')
+
+    if (fieldToArr.includes('.')) {
+        // get word before and after fullstop
+        const stopIndex = fieldToArr.indexOf('.')
+        const wordBeforeStop = fieldToArr.slice(0, stopIndex).join('')
+        const wordAfterStop = fieldToArr.slice(stopIndex + 1).join('')
+
+        const dataToCheck = data[wordBeforeStop][wordAfterStop]
+
+        return conditionCheck(dataToCheck, rule)
+        // return true
     }
 
-    if (rule.condition === 'gt') {
-        return data[rule.field] > rule.condition_value ? true : false;
-    }
+    const dataToCheck = data[rule.field]
+    console.log('A',conditionCheck(dataToCheck, rule))
 
-    if (rule.condition === 'eq') {
-        return data[rule.field] === rule.condition_value ? true : false;
-    }
-
-    if (rule.condition === 'neq') {
-        return data[rule.field] !== rule.condition_value ? true : false;
-    }
-
-    if (rule.condition === 'contains') {
-        const tempArray = []    
-        tempArray.push(data[rule.field])
-        return tempArray.includes(rule.condition_value) ? true : false;
-    }
+    return conditionCheck(dataToCheck, rule)
+    
 }
 
 // Check if rule field is valid
 const isValidRule = (res, payload) => {
-    const rule = payload.rule;
+    const { rule, data } = payload;
 
     const ruleString = JSON.stringify(rule);
     if (isValidJSONString(ruleString)) {
         // Check conditions
+        if (!payload.rule) {
+            return res.status(400).json({
+                "message": `rule is required.`,
+                "status": "error",
+                "data": null
+            })
+        }
+        if (!payload.data) {
+            return res.status(400).json({
+                "message": `data is required.`,
+                "status": "error",
+                "data": null
+            })
+        }
+        // Check if rule and data fields are objects
+        if (!isValidData(rule)) {
+            return res.status(400).json({
+                "message": `rule should be a|an object.`,
+                "status": "error",
+                "data": null
+            })
+        }
+        if (!isValidData(data)) {
+            return res.status(400).json({
+                "message": `field should be a|an object.`,
+                "status": "error",
+                "data": null
+            })
+        }
+
         // Check nesting depth level
         if (getFieldNestingLevel(rule) > 2) {
             return res.status(400).json({
@@ -104,17 +179,8 @@ const isValidRule = (res, payload) => {
             }) 
         }
 
-        // Check if rule field is a valid object
-        if (!isValidData(rule.field)) {
-            return res.status(400).json({
-                "message": `${[rule.field]} should be a|an object.`,
-                "status": "error",
-                "data": null
-            })
-        }
-
         // G. Check is Data and Specified Field Match
-        checkRuleDataMatch(res, payload)
+        checkRuleDataMatch(res, payload) 
         return true
     
     } else {
@@ -124,63 +190,44 @@ const isValidRule = (res, payload) => {
             "data": null
         })
     }
-
 }
 
+// Validate Rule and Data Field
 const validateJSON = (req, res, payload) => {
-// Check if the rule and data fields are in the payload
-    console.log('Validating JSON')
-    if (!payload.rule) {
-        return res.status(400).json({
-            "message": `${[payload.rule]} is required.`,
-            "status": "error",
-            "data": null
-        })
-    }
-    if (!payload.data) {
-        return res.status(400).json({
-            "message": `${[payload.data]} is required.`,
-            "status": "error",
-            "data": null
-        })
-    }
-    // Valid Rule and Data Field
-    if (isValidRule(res, payload) && isValidData(payload.data)) {
-        // Check conditions against Rule and Return response
-        const { rule, data } = payload
-        if (validateRule(res, payload)) {
-            return res.status(200).json(
-                {
-                    "message": "field missions successfully validated.",
-                    "status": "success",
-                    "data": {
-                      "validation": {
-                        "error": false,
-                        "field": rule.field,
-                        "field_value": data[rule.field],
-                        "condition": rule.condition,
-                        "condition_value": rule.condition_value
-                      }
+    // Check conditions against Rule and Return response
+    const { rule, data } = payload
+    console.log('Validate Rule Conditions', validateRuleConditions(payload))
+    if (isValidRule(res, payload) && validateRuleConditions(payload)) {
+        return res.status(200).json(
+            {
+                "message": "field missions successfully validated.",
+                "status": "success",
+                "data": {
+                    "validation": {
+                    "error": false,
+                    "field": rule.field,
+                    "field_value": data[rule.field],
+                    "condition": rule.condition,
+                    "condition_value": rule.condition_value
                     }
                 }
-            )
-        } else {
-            return res.status(400).json(
-                {
-                    "message": "field missions failed validation.",
-                    "status": "error",
-                    "data": {
-                      "validation": {
-                        "error": true,
-                        "field": rule.field,
-                        "field_value": data[rule.field],
-                        "condition": rule.condition,
-                        "condition_value": rule.condition_value
-                      }
+            }
+        )
+    } else {
+        return res.status(400).json(
+            {
+                "message": "field missions failed validation.",
+                "status": "error",
+                "data": {
+                    "validation": {
+                    "error": true,
+                    "field": rule.field,
+                    "field_value": data[rule.field],
+                    "condition": rule.condition,
+                    "condition_value": rule.condition_value
                     }
-                  }
-            )
-        }
+                }
+            })
     }
 }
 
